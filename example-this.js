04fc2384cb013d6b7cca7b5ab54f0eb9f10a4944
gyaresu <me@gareth.com.au>
2015-04-23 13:09:56 -0700

    bindings

--- example-this.js
+++ example-this.js
@@ -1,19 +1,22 @@
 function identify() {
-    console.log(this);
+    // console.log(this);
     return this.name.toUpperCase();
 }
 
 function speak() {
+    // console.log('This is this: ', this);
     var greeting = "Hello, I'm " + identify.call( this );
-    console.log( greeting );
+    // console.log( greeting );
 }
 
 var me = {
-    name: "Kyle"
+    name: "Kyle",
+    age: 30
 };
 
 var you = {
-    name: "Reader"
+    name: "Reader",
+    age: 100
 };
 
 identify.call( me ); // KYLE
